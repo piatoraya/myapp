5f98a43e140fbd6265c8ff34dcd33dcea7c4d79b
piatoraya <meyshaldyampangp@gmail.com>
2023-12-12 23:07:35 +0800

    update

--- views/header.php
+++ views/header.php
@@ -19,7 +19,7 @@
 <!-- Site wrapper -->
 <div class="wrapper">
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-dark">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -28,15 +28,15 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="<?= App::baseUrl('dashboard') ?>" class="nav-link">Home</a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?= App::baseUrl('login/logout') ?>" class="nav-link">Logout</a>
-      </li>
+      
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
-      
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="<?= App::baseUrl('login/logout') ?>" class="nav-link">Logout</a>
+      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
